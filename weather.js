6cbd9d3d96c801c0c9641e325169d7b1b186be93
Lauren Main <lauren.main28@gmail.com>
2022-08-16 22:14:13 -0700

    refactors using starter code

--- weather.js
+++ weather.js
@@ -1,26 +1,49 @@
-"use strict";
+'use strict';
+let cache = require('./cache.js');
 
-const axios = require("axios");
+const axios = require('axios');
 
-async function getForecast(request, response, next) {
-  const { lat, lon } = request.query;
+// async 
+function getForecast(lat,lon) {
+  const key = 'weather-'+ lat+ lon; 
   // console.log('query', request);
   const url = `https://api.weatherbit.io/v2.0/forecast/daily?days=5&units=I&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url)
+      .then(weatherResponse => parseWeather(weatherResponse.data));
+  }
+  return cache[key].data;
+}
+//   axios
+//     .get(url)
+//     .then((weatherResponse) => {
+//       console.log('results', weatherResponse.data);
+//       const weatherArray = weatherResponse.data.data.map(
+//         (weather) => new Forecast(weather)
+//       );
+//       console.log("TEST", weatherArray);
+//       response.status(200).send(weatherArray);
+//     })
+//     .catch((error) => {
+//       next(error);
+//       console.log("am I running?");
+//     });
+// }
 
-  axios
-    .get(url)
-    .then((weatherResponse) => {
-      console.log("results", weatherResponse.data);
-      const weatherArray = weatherResponse.data.data.map(
-        (weather) => new Forecast(weather)
-      );
-      console.log("TEST", weatherArray);
-      response.status(200).send(weatherArray);
-    })
-    .catch((error) => {
-      next(error);
-      console.log("am I running?");
+function parseWeather(weatherResponse) {
+  try {
+    const weatherArray = weatherResponse.data.map(weather => {
+      return new Forecast(weather);
     });
+    return Promise.resolve(weatherArray);
+  } catch (e) {
+    return Promise.reject(e);
+  }
 }
 
 class Forecast {
